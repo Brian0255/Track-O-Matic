1fde7f6ee2af64b19412e6035eacd410c126ff34
Brian0255 <brian.neveu2@gmail.com>
2023-07-18 12:12:16 -0400

    add new checks for collectibles

--- DK64PointsTracker/Data/ImportantCheckList.cs
+++ DK64PointsTracker/Data/ImportantCheckList.cs
@@ -115,7 +115,7 @@ namespace DK64PointsTracker
         {ItemName.CHUNKY_ISLES_GBS, new ImportantCheck(ItemName.CHUNKY_JAPES_GBS, ItemType.GOLDEN_BANANA)},
         {ItemName.CHUNKY_HELM_GBS, new ImportantCheck(ItemName.CHUNKY_JAPES_GBS, ItemType.GOLDEN_BANANA)},
 
-         {ItemName.DONKEY_JAPES_BP, new ImportantCheck(ItemName.DONKEY_JAPES_BP, ItemType.DONKEY_BLUEPRINT)},
+        {ItemName.DONKEY_JAPES_BP, new ImportantCheck(ItemName.DONKEY_JAPES_BP, ItemType.DONKEY_BLUEPRINT)},
         {ItemName.DONKEY_AZTEC_BP, new ImportantCheck(ItemName.DONKEY_AZTEC_BP, ItemType.DONKEY_BLUEPRINT)},
         {ItemName.DONKEY_FACTORY_BP, new ImportantCheck(ItemName.DONKEY_FACTORY_BP, ItemType.DONKEY_BLUEPRINT)},
         {ItemName.DONKEY_GALLEON_BP, new ImportantCheck(ItemName.DONKEY_GALLEON_BP, ItemType.DONKEY_BLUEPRINT)},
@@ -123,12 +123,154 @@ namespace DK64PointsTracker
         {ItemName.DONKEY_CAVES_BP, new ImportantCheck(ItemName.DONKEY_CAVES_BP, ItemType.DONKEY_BLUEPRINT)},
         {ItemName.DONKEY_CASTLE_BP, new ImportantCheck(ItemName.DONKEY_CASTLE_BP, ItemType.DONKEY_BLUEPRINT)},
         {ItemName.DONKEY_ISLES_BP, new ImportantCheck(ItemName.DONKEY_ISLES_BP, ItemType.DONKEY_BLUEPRINT)},
-      /*
+
+        {ItemName.DIDDY_JAPES_BP, new ImportantCheck(ItemName.DIDDY_JAPES_BP, ItemType.DIDDY_BLUEPRINT)},
+        {ItemName.DIDDY_AZTEC_BP, new ImportantCheck(ItemName.DIDDY_AZTEC_BP, ItemType.DIDDY_BLUEPRINT)},
+        {ItemName.DIDDY_FACTORY_BP, new ImportantCheck(ItemName.DIDDY_FACTORY_BP, ItemType.DIDDY_BLUEPRINT)},
+        {ItemName.DIDDY_GALLEON_BP, new ImportantCheck(ItemName.DIDDY_GALLEON_BP, ItemType.DIDDY_BLUEPRINT)},
+        {ItemName.DIDDY_FOREST_BP, new ImportantCheck(ItemName.DIDDY_FOREST_BP, ItemType.DIDDY_BLUEPRINT)},
+        {ItemName.DIDDY_CAVES_BP, new ImportantCheck(ItemName.DIDDY_CAVES_BP, ItemType.DIDDY_BLUEPRINT)},
+        {ItemName.DIDDY_CASTLE_BP, new ImportantCheck(ItemName.DIDDY_CASTLE_BP, ItemType.DIDDY_BLUEPRINT)},
+        {ItemName.DIDDY_ISLES_BP, new ImportantCheck(ItemName.DIDDY_ISLES_BP, ItemType.DIDDY_BLUEPRINT)},
+
+        {ItemName.LANKY_JAPES_BP, new ImportantCheck(ItemName.LANKY_JAPES_BP, ItemType.LANKY_BLUEPRINT)},
+        {ItemName.LANKY_AZTEC_BP, new ImportantCheck(ItemName.LANKY_AZTEC_BP, ItemType.LANKY_BLUEPRINT)},
+        {ItemName.LANKY_FACTORY_BP, new ImportantCheck(ItemName.LANKY_FACTORY_BP, ItemType.LANKY_BLUEPRINT)},
+        {ItemName.LANKY_GALLEON_BP, new ImportantCheck(ItemName.LANKY_GALLEON_BP, ItemType.LANKY_BLUEPRINT)},
+        {ItemName.LANKY_FOREST_BP, new ImportantCheck(ItemName.LANKY_FOREST_BP, ItemType.LANKY_BLUEPRINT)},
+        {ItemName.LANKY_CAVES_BP, new ImportantCheck(ItemName.LANKY_CAVES_BP, ItemType.LANKY_BLUEPRINT)},
+        {ItemName.LANKY_CASTLE_BP, new ImportantCheck(ItemName.LANKY_CASTLE_BP, ItemType.LANKY_BLUEPRINT)},
+        {ItemName.LANKY_ISLES_BP, new ImportantCheck(ItemName.LANKY_ISLES_BP, ItemType.LANKY_BLUEPRINT)},
+
+        {ItemName.TINY_JAPES_BP, new ImportantCheck(ItemName.TINY_JAPES_BP, ItemType.TINY_BLUEPRINT)},
+        {ItemName.TINY_AZTEC_BP, new ImportantCheck(ItemName.TINY_AZTEC_BP, ItemType.TINY_BLUEPRINT)},
+        {ItemName.TINY_FACTORY_BP, new ImportantCheck(ItemName.TINY_FACTORY_BP, ItemType.TINY_BLUEPRINT)},
+        {ItemName.TINY_GALLEON_BP, new ImportantCheck(ItemName.TINY_GALLEON_BP, ItemType.TINY_BLUEPRINT)},
+        {ItemName.TINY_FOREST_BP, new ImportantCheck(ItemName.TINY_FOREST_BP, ItemType.TINY_BLUEPRINT)},
+        {ItemName.TINY_CAVES_BP, new ImportantCheck(ItemName.TINY_CAVES_BP, ItemType.TINY_BLUEPRINT)},
+        {ItemName.TINY_CASTLE_BP, new ImportantCheck(ItemName.TINY_CASTLE_BP, ItemType.TINY_BLUEPRINT)},
+        {ItemName.TINY_ISLES_BP, new ImportantCheck(ItemName.TINY_ISLES_BP, ItemType.TINY_BLUEPRINT)},
+
+        {ItemName.CHUNKY_JAPES_BP, new ImportantCheck(ItemName.CHUNKY_JAPES_BP, ItemType.CHUNKY_BLUEPRINT)},
+        {ItemName.CHUNKY_AZTEC_BP, new ImportantCheck(ItemName.CHUNKY_AZTEC_BP, ItemType.CHUNKY_BLUEPRINT)},
+        {ItemName.CHUNKY_FACTORY_BP, new ImportantCheck(ItemName.CHUNKY_FACTORY_BP, ItemType.CHUNKY_BLUEPRINT)},
+        {ItemName.CHUNKY_GALLEON_BP, new ImportantCheck(ItemName.CHUNKY_GALLEON_BP, ItemType.CHUNKY_BLUEPRINT)},
+        {ItemName.CHUNKY_FOREST_BP, new ImportantCheck(ItemName.CHUNKY_FOREST_BP, ItemType.CHUNKY_BLUEPRINT)},
+        {ItemName.CHUNKY_CAVES_BP, new ImportantCheck(ItemName.CHUNKY_CAVES_BP, ItemType.CHUNKY_BLUEPRINT)},
+        {ItemName.CHUNKY_CASTLE_BP, new ImportantCheck(ItemName.CHUNKY_CASTLE_BP, ItemType.CHUNKY_BLUEPRINT)},
+        {ItemName.CHUNKY_ISLES_BP, new ImportantCheck(ItemName.CHUNKY_ISLES_BP, ItemType.CHUNKY_BLUEPRINT)},
+
+        {ItemName.DONKEY_JAPES_MEDAL, new ImportantCheck(ItemName.DONKEY_JAPES_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.DONKEY_AZTEC_MEDAL, new ImportantCheck(ItemName.DONKEY_AZTEC_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.DONKEY_FACTORY_MEDAL, new ImportantCheck(ItemName.DONKEY_FACTORY_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.DONKEY_GALLEON_MEDAL, new ImportantCheck(ItemName.DONKEY_GALLEON_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.DONKEY_FOREST_MEDAL, new ImportantCheck(ItemName.DONKEY_FOREST_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.DONKEY_CAVES_MEDAL, new ImportantCheck(ItemName.DONKEY_CAVES_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.DONKEY_CASTLE_MEDAL, new ImportantCheck(ItemName.DONKEY_CASTLE_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.DONKEY_HELM_MEDAL, new ImportantCheck(ItemName.DONKEY_HELM_MEDAL, ItemType.BANANA_MEDAL)},
+
+        {ItemName.DIDDY_JAPES_MEDAL, new ImportantCheck(ItemName.DIDDY_JAPES_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.DIDDY_AZTEC_MEDAL, new ImportantCheck(ItemName.DIDDY_AZTEC_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.DIDDY_FACTORY_MEDAL, new ImportantCheck(ItemName.DIDDY_FACTORY_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.DIDDY_GALLEON_MEDAL, new ImportantCheck(ItemName.DIDDY_GALLEON_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.DIDDY_FOREST_MEDAL, new ImportantCheck(ItemName.DIDDY_FOREST_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.DIDDY_CAVES_MEDAL, new ImportantCheck(ItemName.DIDDY_CAVES_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.DIDDY_CASTLE_MEDAL, new ImportantCheck(ItemName.DIDDY_CASTLE_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.DIDDY_HELM_MEDAL, new ImportantCheck(ItemName.DIDDY_HELM_MEDAL, ItemType.BANANA_MEDAL)},
+
+        {ItemName.LANKY_JAPES_MEDAL, new ImportantCheck(ItemName.LANKY_JAPES_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.LANKY_AZTEC_MEDAL, new ImportantCheck(ItemName.LANKY_AZTEC_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.LANKY_FACTORY_MEDAL, new ImportantCheck(ItemName.LANKY_FACTORY_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.LANKY_GALLEON_MEDAL, new ImportantCheck(ItemName.LANKY_GALLEON_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.LANKY_FOREST_MEDAL, new ImportantCheck(ItemName.LANKY_FOREST_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.LANKY_CAVES_MEDAL, new ImportantCheck(ItemName.LANKY_CAVES_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.LANKY_CASTLE_MEDAL, new ImportantCheck(ItemName.LANKY_CASTLE_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.LANKY_HELM_MEDAL, new ImportantCheck(ItemName.LANKY_HELM_MEDAL, ItemType.BANANA_MEDAL)},
+
+        {ItemName.TINY_JAPES_MEDAL, new ImportantCheck(ItemName.TINY_JAPES_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.TINY_AZTEC_MEDAL, new ImportantCheck(ItemName.TINY_AZTEC_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.TINY_FACTORY_MEDAL, new ImportantCheck(ItemName.TINY_FACTORY_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.TINY_GALLEON_MEDAL, new ImportantCheck(ItemName.TINY_GALLEON_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.TINY_FOREST_MEDAL, new ImportantCheck(ItemName.TINY_FOREST_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.TINY_CAVES_MEDAL, new ImportantCheck(ItemName.TINY_CAVES_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.TINY_CASTLE_MEDAL, new ImportantCheck(ItemName.TINY_CASTLE_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.TINY_HELM_MEDAL, new ImportantCheck(ItemName.TINY_HELM_MEDAL, ItemType.BANANA_MEDAL)},
+
+        {ItemName.CHUNKY_JAPES_MEDAL, new ImportantCheck(ItemName.CHUNKY_JAPES_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.CHUNKY_AZTEC_MEDAL, new ImportantCheck(ItemName.CHUNKY_AZTEC_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.CHUNKY_FACTORY_MEDAL, new ImportantCheck(ItemName.CHUNKY_FACTORY_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.CHUNKY_GALLEON_MEDAL, new ImportantCheck(ItemName.CHUNKY_GALLEON_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.CHUNKY_FOREST_MEDAL, new ImportantCheck(ItemName.CHUNKY_FOREST_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.CHUNKY_CAVES_MEDAL, new ImportantCheck(ItemName.CHUNKY_CAVES_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.CHUNKY_CASTLE_MEDAL, new ImportantCheck(ItemName.CHUNKY_CASTLE_MEDAL, ItemType.BANANA_MEDAL)},
+        {ItemName.CHUNKY_HELM_MEDAL, new ImportantCheck(ItemName.CHUNKY_HELM_MEDAL, ItemType.BANANA_MEDAL)},
+
+        {ItemName.JAPES_BATTLE_CROWN, new ImportantCheck(ItemName.JAPES_BATTLE_CROWN, ItemType.BATTLE_CROWN) },
+        {ItemName.AZTEC_BATTLE_CROWN, new ImportantCheck(ItemName.AZTEC_BATTLE_CROWN, ItemType.BATTLE_CROWN) },
+        {ItemName.FACTORY_BATTLE_CROWN, new ImportantCheck(ItemName.FACTORY_BATTLE_CROWN, ItemType.BATTLE_CROWN) },
+        {ItemName.GALLEON_BATTLE_CROWN, new ImportantCheck(ItemName.GALLEON_BATTLE_CROWN, ItemType.BATTLE_CROWN) },
+        {ItemName.FOREST_BATTLE_CROWN, new ImportantCheck(ItemName.FOREST_BATTLE_CROWN, ItemType.BATTLE_CROWN) },
+        {ItemName.CAVES_BATTLE_CROWN, new ImportantCheck(ItemName.CAVES_BATTLE_CROWN, ItemType.BATTLE_CROWN) },
+        {ItemName.CASTLE_BATTLE_CROWN, new ImportantCheck(ItemName.CASTLE_BATTLE_CROWN, ItemType.BATTLE_CROWN) },
+        {ItemName.ISLES_BATTLE_CROWN_1, new ImportantCheck(ItemName.ISLES_BATTLE_CROWN_1, ItemType.BATTLE_CROWN) },
+        {ItemName.ISLES_BATTLE_CROWN_2, new ImportantCheck(ItemName.ISLES_BATTLE_CROWN_2, ItemType.BATTLE_CROWN) },
+        {ItemName.HELM_BATTLE_CROWN, new ImportantCheck(ItemName.HELM_BATTLE_CROWN, ItemType.BATTLE_CROWN) },
+
+
+
+        {ItemName.JAPES_FAIRY_1, new ImportantCheck(ItemName.JAPES_FAIRY_1, ItemType.FAIRY) },
+        {ItemName.JAPES_FAIRY_2, new ImportantCheck(ItemName.JAPES_FAIRY_2, ItemType.FAIRY) },
+
+        {ItemName.AZTEC_FAIRY_1, new ImportantCheck(ItemName.AZTEC_FAIRY_1, ItemType.FAIRY) },
+        {ItemName.AZTEC_FAIRY_2, new ImportantCheck(ItemName.AZTEC_FAIRY_2, ItemType.FAIRY) },
+
+        {ItemName.FACTORY_FAIRY_1, new ImportantCheck(ItemName.FACTORY_FAIRY_1, ItemType.FAIRY) },
+        {ItemName.FACTORY_FAIRY_2, new ImportantCheck(ItemName.FACTORY_FAIRY_2, ItemType.FAIRY) },
+
+        {ItemName.GALLEON_FAIRY_1, new ImportantCheck(ItemName.GALLEON_FAIRY_1, ItemType.FAIRY) },
+        {ItemName.GALLEON_FAIRY_2, new ImportantCheck(ItemName.GALLEON_FAIRY_2, ItemType.FAIRY) },
+
+        {ItemName.FOREST_FAIRY_1, new ImportantCheck(ItemName.FOREST_FAIRY_1, ItemType.FAIRY) },
+        {ItemName.FOREST_FAIRY_2, new ImportantCheck(ItemName.FOREST_FAIRY_2, ItemType.FAIRY) },
+
+        {ItemName.CAVES_FAIRY_1, new ImportantCheck(ItemName.CAVES_FAIRY_1, ItemType.FAIRY) },
+        {ItemName.CAVES_FAIRY_2, new ImportantCheck(ItemName.CAVES_FAIRY_2, ItemType.FAIRY) },
+
+        {ItemName.CASTLE_FAIRY_1, new ImportantCheck(ItemName.CASTLE_FAIRY_1, ItemType.FAIRY) },
+        {ItemName.CASTLE_FAIRY_2, new ImportantCheck(ItemName.CASTLE_FAIRY_2, ItemType.FAIRY) },
+
+        {ItemName.HELM_FAIRY_1, new ImportantCheck(ItemName.HELM_FAIRY_1, ItemType.FAIRY) },
+        {ItemName.HELM_FAIRY_2, new ImportantCheck(ItemName.HELM_FAIRY_2, ItemType.FAIRY) },
+
+        {ItemName.ISLES_FAIRY_1, new ImportantCheck(ItemName.ISLES_FAIRY_1, ItemType.FAIRY) },
+        {ItemName.ISLES_FAIRY_2, new ImportantCheck(ItemName.ISLES_FAIRY_2, ItemType.FAIRY) },
+        {ItemName.ISLES_FAIRY_3, new ImportantCheck(ItemName.ISLES_FAIRY_3, ItemType.FAIRY) },
+        {ItemName.ISLES_FAIRY_4, new ImportantCheck(ItemName.ISLES_FAIRY_4, ItemType.FAIRY) },
+
+
+
+        {ItemName.RAINBOW_COIN_1, new ImportantCheck(ItemName.RAINBOW_COIN_1, ItemType.RAINBOW_COIN) },
+        {ItemName.RAINBOW_COIN_2, new ImportantCheck(ItemName.RAINBOW_COIN_2, ItemType.RAINBOW_COIN) },
+        {ItemName.RAINBOW_COIN_3, new ImportantCheck(ItemName.RAINBOW_COIN_3, ItemType.RAINBOW_COIN) },
+        {ItemName.RAINBOW_COIN_4, new ImportantCheck(ItemName.RAINBOW_COIN_4, ItemType.RAINBOW_COIN) },
+        {ItemName.RAINBOW_COIN_5, new ImportantCheck(ItemName.RAINBOW_COIN_5, ItemType.RAINBOW_COIN) },
+        {ItemName.RAINBOW_COIN_6, new ImportantCheck(ItemName.RAINBOW_COIN_6, ItemType.RAINBOW_COIN) },
+        {ItemName.RAINBOW_COIN_7, new ImportantCheck(ItemName.RAINBOW_COIN_7, ItemType.RAINBOW_COIN) },
+        {ItemName.RAINBOW_COIN_8, new ImportantCheck(ItemName.RAINBOW_COIN_8, ItemType.RAINBOW_COIN) },
+        {ItemName.RAINBOW_COIN_9, new ImportantCheck(ItemName.RAINBOW_COIN_9, ItemType.RAINBOW_COIN) },
+        {ItemName.RAINBOW_COIN_10, new ImportantCheck(ItemName.RAINBOW_COIN_10, ItemType.RAINBOW_COIN) },
+        {ItemName.RAINBOW_COIN_11, new ImportantCheck(ItemName.RAINBOW_COIN_11, ItemType.RAINBOW_COIN) },
+        {ItemName.RAINBOW_COIN_12, new ImportantCheck(ItemName.RAINBOW_COIN_12, ItemType.RAINBOW_COIN) },
+        {ItemName.RAINBOW_COIN_13, new ImportantCheck(ItemName.RAINBOW_COIN_13, ItemType.RAINBOW_COIN) },
+        {ItemName.RAINBOW_COIN_14, new ImportantCheck(ItemName.RAINBOW_COIN_14, ItemType.RAINBOW_COIN) },
+        {ItemName.RAINBOW_COIN_15, new ImportantCheck(ItemName.RAINBOW_COIN_15, ItemType.RAINBOW_COIN) },
+        {ItemName.RAINBOW_COIN_16, new ImportantCheck(ItemName.RAINBOW_COIN_16, ItemType.RAINBOW_COIN) },
+
         {ItemName.PEARL_1, new ImportantCheck(ItemName.PEARL_1, ItemType.PEARL)},
         {ItemName.PEARL_2, new ImportantCheck(ItemName.PEARL_2, ItemType.PEARL)},
         {ItemName.PEARL_3, new ImportantCheck(ItemName.PEARL_3, ItemType.PEARL)},
         {ItemName.PEARL_4, new ImportantCheck(ItemName.PEARL_4, ItemType.PEARL)},
-        {ItemName.PEARL_5, new ImportantCheck(ItemName.PEARL_5, ItemType.PEARL)},*/
+        {ItemName.PEARL_5, new ImportantCheck(ItemName.PEARL_5, ItemType.PEARL)},
        };
     }
 
