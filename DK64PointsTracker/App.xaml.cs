88de7acf52f88afaebaa9aec0721c9a4897ff000
Brian0255 <brian.neveu2@gmail.com>
2023-08-07 12:38:55 -0400

    minor changes

--- DK64PointsTracker/App.xaml.cs
+++ DK64PointsTracker/App.xaml.cs
@@ -13,28 +13,18 @@ namespace DK64PointsTracker
     /// </summary>
     public partial class App : Application
     {
-        public static Log logger;
 
         App()
         {
-            this.Dispatcher.UnhandledException += OnDispatcherUnhandledException;
-            try
-            {
-                logger = new Log(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\DK64PointsTracker\\log.txt");
-            }
-            catch
-            { };
+            Dispatcher.UnhandledException += OnDispatcherUnhandledException;
         }
 
         private void App_Exit(object sender, ExitEventArgs e)
         {
-            if (App.logger != null)
-                logger.Close();
         }
 
         void OnDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
-            (MainWindow as MainWindow).Save("pape-tracker-autosave.txt");
         }
     }
 }
