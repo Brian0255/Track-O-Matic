9c8495c002be7e97f943a479c14f8bae1ee3c4bd
Brian0255 <brian.neveu2@gmail.com>
2023-08-07 12:39:03 -0400

    create new vialcolor enum

---
+++ DK64PointsTracker/Data/VialColor.cs
@@ -0,0 +1,18 @@
+﻿namespace DK64PointsTracker
+{
+    public enum VialColor
+    {
+        CLEAR,
+
+        YELLOW,
+        RED,
+        BLUE,
+        PURPLE,
+        GREEN,
+
+        KEY,
+        KONG,
+
+        NONE
+    }
+}
