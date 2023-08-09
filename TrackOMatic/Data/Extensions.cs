700db531e1b415318316268ecad3132e49e73e76
Brian0255 <brian.neveu2@gmail.com>
2023-08-08 21:29:07 -0400

    shuffled method for hitlist generation

---
+++ TrackOMatic/Data/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrackOMatic
+{
+    static class Extensions
+    {
+        private static Random random = new();
+
+        public static void Shuffle<T>(this IList<T> list, int seed)
+        {
+            random = new(seed);
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
