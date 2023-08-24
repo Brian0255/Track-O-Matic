1b6542784f47d9b6cafe33a9aa93788be525ac74
Brian0255 <brian.neveu2@gmail.com>
2023-08-24 01:30:43 -0400

    add bw images

--- TrackOMatic/Images/dk64/darkener.py
+++ TrackOMatic/Images/dk64/darkener.py
@@ -1,5 +1,43 @@
 import os
 from PIL import Image, ImageEnhance
+import numpy as np
+from PIL import Image, ImageFilter
+
+
+def makeShadow(image, iterations, border, offset, backgroundColour, shadowColour):
+    # image: base image to give a drop shadow
+    # iterations: number of times to apply the blur filter to the shadow
+    # border: border to give the image to leave space for the shadow
+    # offset: offset of the shadow as [x,y]
+    # backgroundCOlour: colour of the background
+    # shadowColour: colour of the drop shadow
+
+    # Calculate the size of the shadow's image
+    fullWidth = image.size[0] + abs(offset[0]) + 2 * border
+    fullHeight = image.size[1] + abs(offset[1]) + 2 * border
+
+    # Create the shadow's image. Match the parent image's mode.
+    shadow = Image.new(image.mode, (fullWidth, fullHeight), backgroundColour)
+
+    # Place the shadow, with the required offset
+    shadowLeft = border + max(offset[0], 0)  # if <0, push the rest of the image right
+    shadowTop = border + max(offset[1], 0)  # if <0, push the rest of the image down
+    # Paste in the constant colour
+    shadow.paste(shadowColour,
+                 [shadowLeft, shadowTop,
+                  shadowLeft + image.size[0],
+                  shadowTop + image.size[1]])
+
+    # Apply the BLUR filter repeatedly
+    for i in range(iterations):
+        shadow = shadow.filter(ImageFilter.BLUR)
+
+    # Paste the original image on top of the shadow
+    imgLeft = border - min(offset[0], 0)  # if the shadow offset was <0, push right
+    imgTop = border - min(offset[1], 0)  # if the shadow offset was <0, push down
+    shadow.paste(image, (imgLeft, imgTop))
+
+    return shadow
 
 def makeBW():
     for filename in os.listdir("bw"):
