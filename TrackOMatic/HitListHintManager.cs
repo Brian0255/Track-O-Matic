ed572c0c9d112fd2c05a5c2a0d13eb7414d20808
Brian0255 <brian.neveu2@gmail.com>
2023-08-25 21:05:59 -0400

    add class to manage the new hints for hitlist every time you get 5 gbs

---
+++ TrackOMatic/HitListHintManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.IO;
+using Microsoft.Win32;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Timers;
+using TrackOMatic.Properties;
+
+namespace TrackOMatic
+{
+    public class HitListHintManager
+    {
+        public MainWindow MainWindow { get; }
+        private int NextHintBarrier = 5;
+        private int CurrentIndex = 0;
+        private List<ItemName> ItemsToPullFrom;
+        private Dictionary<ItemName, RegionName> StartingItems;
+        private Dictionary<ItemName, RegionName> TrainingItems;
+        private bool SpoilerLoaded = false;
+
+        public HitListHintManager(MainWindow mainWindow) 
+        {
+            MainWindow = mainWindow;
+        }
+
+        private bool ShouldExclude(ItemName itemName)
+        {
+            if (StartingItems.ContainsKey(itemName)) return true;
+            if (TrainingItems.ContainsKey(itemName)) return true;
+            return false;
+        }
+
+        public void InitializeFromSpoiler(Dictionary<ItemName, RegionName> startingItems, Dictionary<ItemName, RegionName> trainingItems)
+        {
+            SpoilerLoaded = true;
+            NextHintBarrier = 5;
+            CurrentIndex = 0;
+            ItemsToPullFrom = new();
+            StartingItems = startingItems;
+            TrainingItems = trainingItems;
+            foreach(var entry in MainWindow.ITEM_NAME_TO_REGION)
+            {
+                var itemName = entry.Key;
+                if (ShouldExclude(itemName)) continue;
+                ItemsToPullFrom.Add(itemName);
+            }
+            ItemsToPullFrom.Shuffle(MainWindow.SpoilerParser.RNGSeed);
+        }
+
+        public ItemName OnGBUpdate(int newTotal)
+        {
+            if (!Settings.Default.HitList) return ItemName.NONE;
+            if(!SpoilerLoaded) return ItemName.NONE;
+            if (newTotal < NextHintBarrier) return ItemName.NONE;
+            CurrentIndex = (NextHintBarrier - 5) / 5;
+            NextHintBarrier += 5;
+            if (CurrentIndex > ItemsToPullFrom.Count - 1) return ItemName.NONE;
+            return ItemsToPullFrom[CurrentIndex];
+        }
+    }
+}
