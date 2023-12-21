1b60878b72d713701f660b6a0c1090e70b484689
Brian0255 <brian.neveu2@gmail.com>
2023-12-21 13:44:21 -0500

    hint type enum

---
+++ TrackOMatic/Data/HintType.cs
@@ -0,0 +1,14 @@
+﻿namespace TrackOMatic
+{
+    public enum HintType 
+    { 
+        PATH,
+        WAY_OF_THE_HOARD,
+        FOOLISH_REGION,
+        PATHLESS_ITEM,
+        REGION_POTION_COUNT,
+        UNHINTED,
+        MISC,
+        KONGS,
+    }
+}
