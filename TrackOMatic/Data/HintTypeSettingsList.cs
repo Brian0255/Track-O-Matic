c053f58df3751147a5cf797c289357a12592aefc
Brian0255 <brian.neveu2@gmail.com>
2023-12-21 13:44:48 -0500

    map hint type to settings for hint ui stuff

---
+++ TrackOMatic/Data/HintTypeSettingsList.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace TrackOMatic
+{
+    public static class HintTypeSettingsList
+    {
+        public static readonly Dictionary<HintType, HintTypeSettings> SETTINGS = new(){
+            {HintType.PATH, new HintTypeSettings(Visibility.Visible, Visibility.Visible, HintSuggestion.LOCATION) },
+            {HintType.KONGS, new HintTypeSettings(Visibility.Hidden, Visibility.Hidden, HintSuggestion.NONE) },
+            {HintType.WAY_OF_THE_HOARD, new HintTypeSettings(Visibility.Hidden, Visibility.Visible, HintSuggestion.NONE) },
+            {HintType.REGION_POTION_COUNT, new HintTypeSettings(Visibility.Hidden, Visibility.Hidden, HintSuggestion.LOCATION) },
+            {HintType.FOOLISH_REGION, new HintTypeSettings(Visibility.Hidden, Visibility.Hidden, HintSuggestion.LOCATION) },
+            {HintType.PATHLESS_ITEM, new HintTypeSettings(Visibility.Hidden, Visibility.Hidden, HintSuggestion.MOVE) },
+            {HintType.UNHINTED, new HintTypeSettings(Visibility.Hidden, Visibility.Visible, HintSuggestion.LOCATION) },
+            {HintType.MISC, new HintTypeSettings(Visibility.Hidden, Visibility.Hidden, HintSuggestion.NONE) }
+       };
+    }
+}
