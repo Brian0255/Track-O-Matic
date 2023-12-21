30172a48ab9e0efef07ada1f1cf992354cb7064d
Brian0255 <brian.neveu2@gmail.com>
2023-12-21 13:43:51 -0500

    move records to data folder because that makes more sense

--- TrackOMatic/Data/Records.cs
+++ TrackOMatic/Data/Records.cs
@@ -1,8 +1,11 @@
 ﻿using System.Diagnostics;
+using System.Collections.Generic;
+using System.Windows;
 
 namespace TrackOMatic
 {
     public record GameVerificationInfo(uint TargetAddress, int TotalBits, uint TargetValue);
     public record AttachedProcessInfo(Process Process, uint StartAddress);
     public record OffsetInfoEntry(ItemName ItemName, uint Offset, int TotalBits, int Bitmask = -1, int TargetValue = 0);
+    public record HintTypeSettings(Visibility PathItemsVisible, Visibility FoundItemVisible, HintSuggestion HintSuggestion);
 }
