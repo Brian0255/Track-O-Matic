e3418ee1bf0a4b1d2333e1a284c12d8b26c9f144
Brian0255 <brian.neveu2@gmail.com>
2023-12-21 13:46:50 -0500

    generic user control hint panel for making the displaying of hints very easy

---
+++ TrackOMatic/HintsUI/HintPanel.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrackOMatic
+{
+    public partial class HintPanel : UserControl
+    {
+        public static readonly DependencyProperty HeadingProperty = DependencyProperty.Register("Heading", typeof(string), typeof(HintPanel));
+        public static readonly DependencyProperty LineColorProperty = DependencyProperty.Register("LineColor", typeof(Color), typeof(HintPanel));
+        public static readonly DependencyProperty HintTypeProperty = DependencyProperty.Register("HintType", typeof(HintType), typeof(HintPanel));
+        public string Heading
+        {
+            get { return (string)GetValue(HeadingProperty); }
+            set { SetValue(HeadingProperty, value); }
+        }
+
+        public Color LineColor
+        {
+            get { return (Color)GetValue(LineColorProperty); }
+            set { SetValue(LineColorProperty, value); }
+        }
+
+        public HintType HintType
+        {
+            get { return (HintType)GetValue(HintTypeProperty); }
+            set { SetValue(HintTypeProperty, value); }
+        }
+        public HintPanel()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void RemoveHint(object sender, MouseEventArgs e)
+        {
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                HintInfo hint = (HintInfo)sender;
+                HintList.Children.Remove(hint);
+            }
+        }
+
+        public void OnAddHint(object sender, MouseEventArgs e)
+        {
+            var newHint = new HintInfo(HintType);
+            var insertionPoint = HintList.Children.Count - 1;
+            HintList.Children.Insert(insertionPoint, newHint);
+            newHint.MouseDown += RemoveHint;
+        }
+    }
+}
