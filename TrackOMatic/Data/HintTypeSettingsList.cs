9934f31651cca3e6dd010be209b1f1eae5753578
Brian0255 <brian.neveu2@gmail.com>
2023-12-22 23:41:46 -0500

    adjust a few visibility options

--- TrackOMatic/Data/HintTypeSettingsList.cs
+++ TrackOMatic/Data/HintTypeSettingsList.cs
@@ -10,10 +10,10 @@ namespace TrackOMatic
             {HintType.PATH, new HintTypeSettings(Visibility.Visible, Visibility.Visible, HintSuggestion.LOCATION) },
             {HintType.KONGS, new HintTypeSettings(Visibility.Hidden, Visibility.Hidden, HintSuggestion.NONE) },
             {HintType.WAY_OF_THE_HOARD, new HintTypeSettings(Visibility.Hidden, Visibility.Visible, HintSuggestion.NONE) },
-            {HintType.REGION_POTION_COUNT, new HintTypeSettings(Visibility.Hidden, Visibility.Hidden, HintSuggestion.LOCATION) },
+            {HintType.REGION_POTION_COUNT, new HintTypeSettings(Visibility.Hidden, Visibility.Hidden, HintSuggestion.LOCATION, false, Visibility.Visible) },
             {HintType.FOOLISH_REGION, new HintTypeSettings(Visibility.Hidden, Visibility.Hidden, HintSuggestion.LOCATION) },
-            {HintType.PATHLESS_ITEM, new HintTypeSettings(Visibility.Hidden, Visibility.Hidden, HintSuggestion.MOVE) },
-            {HintType.UNHINTED, new HintTypeSettings(Visibility.Hidden, Visibility.Visible, HintSuggestion.LOCATION) },
+            {HintType.PATHLESS_MOVE, new HintTypeSettings(Visibility.Hidden, Visibility.Hidden, HintSuggestion.MOVE) },
+            {HintType.UNHINTED, new HintTypeSettings(Visibility.Hidden, Visibility.Visible, HintSuggestion.NONE, true) },
             {HintType.MISC, new HintTypeSettings(Visibility.Hidden, Visibility.Hidden, HintSuggestion.NONE) }
        };
     }
