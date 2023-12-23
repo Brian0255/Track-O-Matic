1b9a64a9015b202682723956e2db97861567d372
Brian0255 <brian.neveu2@gmail.com>
2023-12-22 23:44:37 -0500

    create very light object for drop shadow effect

---
+++ TrackOMatic/HintsUI/PathOrFoundItem.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System;
+using System.Windows.Media.Imaging;
+using System.Windows.Forms;
+
+namespace TrackOMatic
+{
+    public partial class PathOrFoundItem : ContentControl
+    {
+        public PathOrFoundItem(Image image)
+        {
+            InitializeComponent();
+            Image.Content = image;
+            DataContext = this;
+        }
+    }
+}
