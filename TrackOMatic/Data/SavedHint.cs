814442cbe2294ab5f42822596291f03749ea348d
Brian0255 <brian.neveu2@gmail.com>
2023-12-24 03:52:35 -0500

    create savedhint object for data saving

---
+++ TrackOMatic/Data/SavedHint.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+using System.Collections.Generic;
+
+namespace TrackOMatic
+{
+    public class SavedHint
+    {
+        public string HintPanelKey { get; }
+        public string LocationText { get; set; }
+        public string PotionCountText { get; set; }
+        public List<ItemName> PathItems { get; set; }
+        public List<ItemName> FoundItems { get; set; }
+
+        public SavedHint(string hintPanelKey, string locationText, string potionCountText, List<ItemName> pathItems, List<ItemName> foundItems)
+        {
+            HintPanelKey = hintPanelKey;
+            LocationText = locationText;
+            PotionCountText = potionCountText;
+            PathItems = pathItems;
+            FoundItems = foundItems;
+        }
+    }
+}
