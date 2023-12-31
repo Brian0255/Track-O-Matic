aec20147a1a5c212ff8668f4d6c34b600ed329e3
Brian0255 <brian.neveu2@gmail.com>
2023-12-31 16:40:53 -0500

    add HintShortcutInfo record

--- TrackOMatic/Data/Records.cs
+++ TrackOMatic/Data/Records.cs
@@ -8,4 +8,5 @@ namespace TrackOMatic
     public record AttachedProcessInfo(Process Process, uint StartAddress);
     public record OffsetInfoEntry(ItemName ItemName, uint Offset, int TotalBits, int Bitmask = -1, int TargetValue = 0);
     public record HintTypeSettings(Visibility PathItemsVisible, Visibility FoundItemVisible, HintSuggestion HintSuggestion, bool PromptForFoundItem=false, Visibility PotionCountVisibility = Visibility.Collapsed);
+    public record HintShortcutInfo(string JSONShortcutsKey, List<string> DefaultSortedList);
 }
