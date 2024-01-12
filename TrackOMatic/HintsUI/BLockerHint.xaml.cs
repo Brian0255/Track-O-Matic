b6f6705df13428a3d47fca6caa0afa2c1e7de6b5
Brian0255 <brian.neveu2@gmail.com>
2024-01-11 21:07:28 -0500

    blockerpanel object

---
+++ TrackOMatic/HintsUI/BLockerHint.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrackOMatic
+{
+    public partial class BLockerHint  : UserControl
+    {
+        public static readonly DependencyProperty RegionNameProperty = DependencyProperty.Register("RegionName", typeof(RegionName), typeof(BLockerHint));
+        public RegionName RegionName
+        {
+            get { return (RegionName)GetValue(RegionNameProperty); }
+            set { SetValue(RegionNameProperty, value); }
+        }  
+
+        public BLockerHint()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            Picture.Source = new BitmapImage(new Uri("../Images/dk64/" + RegionName.ToString().ToLower() + ".png", UriKind.Relative));
+        }
+
+        private void GBCount_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed && GBCount.Text == "?")
+            {
+                GBCount.Text = "";
+            }
+        }
+
+        private void CheckSave()
+        {
+            if (GBCount.Text != "?")
+            {
+                var mainWindow = (MainWindow)Application.Current.MainWindow;
+                mainWindow.DataSaver.Save();
+            }
+        }
+
+        private void GBCount_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Keyboard.ClearFocus();
+                CheckSave();
+            }
+        }
+
+        private void GBCount_LostFocus(object sender, RoutedEventArgs e)
+        {
+            CheckSave();
+        }
+    }
+}
---
+++ TrackOMatic/HintsUI/BLockerPanel.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrackOMatic
+{
+    public partial class BLockerPanel : UserControl
+    {
+        public static readonly DependencyProperty HeadingProperty = DependencyProperty.Register("Heading", typeof(string), typeof(BLockerPanel));
+        public static readonly DependencyProperty LineColorProperty = DependencyProperty.Register("LineColor", typeof(Color), typeof(BLockerPanel));
+        public static readonly DependencyProperty HintTypeProperty = DependencyProperty.Register("HintType", typeof(HintType), typeof(BLockerPanel));
+        public string Heading
+        {
+            get { return (string)GetValue(HeadingProperty); }
+            set { SetValue(HeadingProperty, value); }
+        }
+
+        public Color LineColor
+        {
+            get { return (Color)GetValue(LineColorProperty); }
+            set { SetValue(LineColorProperty, value); }
+        }
+
+        public HintType HintType
+        {
+            get { return (HintType)GetValue(HintTypeProperty); }
+            set { SetValue(HintTypeProperty, value); }
+        }
+
+        public Dictionary<RegionName, BLockerHint> RegionToBLockerHint = new();
+        public BLockerPanel()
+        {
+            InitializeComponent();
+            DataContext = this;
+            foreach(var child in MainGrid.Children)
+            {
+                if(child is BLockerHint hint)
+                {
+                    RegionToBLockerHint[hint.RegionName] = hint;
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            foreach (var hint in RegionToBLockerHint.Values)
+            {
+                hint.GBCount.Text = "?";
+            }
+        }
+
+        public void LoadSavedGBCounts(Dictionary<RegionName, string> GBCounts)
+        {
+            foreach(var entry in GBCounts)
+            {
+                RegionToBLockerHint[entry.Key].GBCount.Text = entry.Value;
+            }
+        }
+
+        public Dictionary<RegionName,string> GetGBCounts()
+        {
+            var GBCounts = new Dictionary<RegionName,string>();
+            foreach(var entry in RegionToBLockerHint)
+            {
+                GBCounts[entry.Key] = entry.Value.GBCount.Text;
+            }
+            return GBCounts;
+        }
+    }
+}
