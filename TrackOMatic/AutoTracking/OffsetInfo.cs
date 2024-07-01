2cb9e66ddc7650f014b97c11a0351a9bbfd36351
Brian0255 <brian.neveu2@gmail.com>
2024-06-30 20:37:05 -0400

    jacob said these numbers are correct so they must be

--- TrackOMatic/AutoTracking/OffsetInfo.cs
+++ TrackOMatic/AutoTracking/OffsetInfo.cs
@@ -320,10 +320,10 @@ namespace TrackOMatic
             new OffsetInfoEntry(ItemName.RAINBOW_COIN_15, 0x7ECEFD,8,16),
             new OffsetInfoEntry(ItemName.RAINBOW_COIN_16, 0x7ECEFD,8,32),
 
-            new OffsetInfoEntry(ItemName.SNIDE, 0x7ECF55,8,128),
-            new OffsetInfoEntry(ItemName.CANDY, 0x7ECF55,8,64),
-            new OffsetInfoEntry(ItemName.FUNKY, 0x7ECF55,8,32),
-            new OffsetInfoEntry(ItemName.CRANKY, 0x7ECF55,8,16),
+            new OffsetInfoEntry(ItemName.SNIDE, 0x7ECF20,8,32),
+            new OffsetInfoEntry(ItemName.CANDY, 0x7ECF20,8,16),
+            new OffsetInfoEntry(ItemName.FUNKY, 0x7ECF20,8,8),
+            new OffsetInfoEntry(ItemName.CRANKY, 0x7ECF20,8,4),
 
         };
     }
