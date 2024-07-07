5cc007cac95629e5db82b2dbf5a03e595732806c
Brian0255 <brian.neveu2@gmail.com>
2024-07-07 05:32:35 -0400

    add ui utility method to ensure a window is visible and move it

---
+++ TrackOMatic/Data/UIUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TrackOMatic
+{
+    static class UIUtils
+    {
+
+        public static void MoveWindowAndEnsureVisibile(Window window, double x, double y)
+        {
+            var currentScreen = System.Windows.Forms.Screen.FromPoint(System.Windows.Forms.Cursor.Position);
+            window.Left = Math.Max(currentScreen.WorkingArea.Left, x);
+            if (window.Left + window.Width > currentScreen.WorkingArea.Left + currentScreen.WorkingArea.Width)
+            {
+                window.Left = currentScreen.WorkingArea.Left + currentScreen.WorkingArea.Width - window.Width;
+            }
+            window.Top = Math.Max(currentScreen.WorkingArea.Top, y);
+            if (window.Top + window.Height > currentScreen.WorkingArea.Top + currentScreen.WorkingArea.Height)
+            {
+                window.Top = currentScreen.WorkingArea.Top + currentScreen.WorkingArea.Height - window.Height;
+            }
+        }
+    }
+}
