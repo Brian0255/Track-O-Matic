7a6180c0c7c662fc7366ef5521017fa2853ffd4e
Brian0255 <brian.neveu2@gmail.com>
2024-07-14 05:46:41 -0400

    add more entries

--- TrackOMatic/Data/SongFormatting.cs
+++ TrackOMatic/Data/SongFormatting.cs
@@ -56,15 +56,20 @@ namespace TrackOMatic
             {"rpg","RPG" },
             {"smrpg:","SMRPG:" },
             {"tmnt","TMNT" },
-            {"yu-gi-oh!","Yu-Gi-Oh!" }
-
+            {"yu-gi-oh!","Yu-Gi-Oh!" },
+            {"mmmenu","MMMenu" },
+            {"cccool","CCCool" },
+            {"dks","DKs" },
+            {"dk's" ,"DK's" },
+            {"dppt","DPPT" },
+            {"gsc","GSC" },
         };
 
         public static readonly Dictionary<string, string> ENTIRE_NAME_REPLACEMENTS = new()
         {
             {"live a live","Live A Live" },
             {"nights into dreams...","NiGHTS into Dreams..." },
-            {"rise of the triad","Rise Of The Triad" }
+            {"rise of the triad","Rise Of The Triad" },
         };
 
         public static string FormatSongString(string songString)
@@ -74,7 +79,7 @@ namespace TrackOMatic
             {
                 return ENTIRE_NAME_REPLACEMENTS[songString];
             }
-            var words = songString.Split(' ');
+            var words = songString.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             var copy = words.ToList();
             for(int i = 0; i < copy.Count; ++i)
             {
