98773f582b116536ec66fc65c7d5a68c9c4f3ed5
Brian0255 <brian.neveu2@gmail.com>
2024-08-27 22:47:28 -0400

    version

--- TrackOMatic/MainWindow.xaml.cs
+++ TrackOMatic/MainWindow.xaml.cs
@@ -464,7 +464,7 @@ namespace TrackOMatic
             AutoUpdater.UpdateFormSize = new System.Drawing.Size(1300, 600);
             AutoUpdater.Icon = Properties.Resources.app.ToBitmap();
 
-            AutoUpdater.InstalledVersion = new Version("1.7.0");
+            AutoUpdater.InstalledVersion = new Version("1.8.0");
 
             AutoUpdater.Start("https://raw.githubusercontent.com/Brian0255/Track-O-Matic/master/TrackOMatic/AutoUpdateInfo.xml");
             if (Settings.Default.DesiredHeight == 0 || Settings.Default.DesiredWidth == 0) return;
