c3d97e09953d0924410286516e181ecd3e744c54
Brian0255 <brian.neveu2@gmail.com>
2024-09-16 15:23:16 -0400

    add saved progress fields for blocker items/helm door items/helm door counts

--- TrackOMatic/Data/SavedProgress.cs
+++ TrackOMatic/Data/SavedProgress.cs
@@ -7,6 +7,9 @@ namespace TrackOMatic
         public string SpoilerLogName { get; }
         public Dictionary<ItemName, SavedItem> SavedItems { get; }
         public Dictionary<RegionName, string> SavedGBCounts { get; set; }
+        public Dictionary<RegionName, int> BLockerImageIndexes { get; set; }
+        public List<int> HelmDoorImageIndexes { get; set; }
+        public List<string> HelmDoorCounts { get; set; }
         public List<SavedHint> SavedHints { get; }
 
         public SavedProgress(string spoilerLogName)
@@ -15,6 +18,9 @@ namespace TrackOMatic
             SavedItems = new();
             SavedHints = new();
             SavedGBCounts = new();
+            BLockerImageIndexes = new();
+            HelmDoorImageIndexes = new();
+            HelmDoorCounts = new();
         }
     }
 }
