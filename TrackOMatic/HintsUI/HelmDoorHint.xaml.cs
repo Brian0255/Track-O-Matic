17b61e9cd76240728b1b52c0559471c9323b0c76
Brian0255 <brian.neveu2@gmail.com>
2024-09-16 15:25:02 -0400

    helm door hint section

---
+++ TrackOMatic/HintsUI/HelmDoorHint.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrackOMatic
+{
+    public partial class HelmDoorHint  : UserControl
+    {
+        public static readonly DependencyProperty LabelTextProperty = DependencyProperty.Register("LabelText", typeof(string), typeof(HelmDoorHint));
+
+        public string LabelText
+        {
+            get { return (string)GetValue(LabelTextProperty); }
+            set { SetValue(LabelTextProperty, value); }
+        }
+        public HelmDoorHint()
+        {
+            InitializeComponent();
+            DataContext = this;
+            List<List<BitmapImage>> BLockerSources = new()
+            {
+                new()
+                {
+                    new BitmapImage( new Uri("images/dk64/gb.png", UriKind.Relative)),
+                    new BitmapImage( new Uri("images/dk64/bp.png", UriKind.Relative)),
+                    new BitmapImage( new Uri("images/dk64/pearl.png", UriKind.Relative)),
+                    new BitmapImage( new Uri("images/dk64/crown.png", UriKind.Relative)),
+                    new BitmapImage( new Uri("images/dk64/bananamedal.png", UriKind.Relative)),
+                    new BitmapImage( new Uri("images/dk64/rainbowcoin.png", UriKind.Relative)),
+                    new BitmapImage( new Uri("images/dk64/fairy.png", UriKind.Relative)),
+                },
+                new()
+                {
+                    new BitmapImage( new Uri("images/dk64/ninrarecoin.png", UriKind.Relative)),
+                    new BitmapImage( new Uri("images/dk64/bean.png", UriKind.Relative)),
+                }
+            };
+            DoorItem.ImageSources = BLockerSources;
+    }
+
+    }
+}
---
+++ TrackOMatic/HintsUI/HelmDoorPanel.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrackOMatic
+{
+    public partial class HelmDoorPanel : UserControl
+    {
+        public static readonly DependencyProperty HeadingProperty = DependencyProperty.Register("Heading", typeof(string), typeof(HelmDoorPanel));
+        public static readonly DependencyProperty LineColorProperty = DependencyProperty.Register("LineColor", typeof(Color), typeof(HelmDoorPanel));
+        public static readonly DependencyProperty HintTypeProperty = DependencyProperty.Register("HintType", typeof(HintType), typeof(HelmDoorPanel));
+        public string Heading
+        {
+            get { return (string)GetValue(HeadingProperty); }
+            set { SetValue(HeadingProperty, value); }
+        }
+
+        public Color LineColor
+        {
+            get { return (Color)GetValue(LineColorProperty); }
+            set { SetValue(LineColorProperty, value); }
+        }
+
+        public HintType HintType
+        {
+            get { return (HintType)GetValue(HintTypeProperty); }
+            set { SetValue(HintTypeProperty, value); }
+        }
+
+        public List<int> ItemImageIndexes { get; private set; }
+        private List<HelmDoorHint> hints;
+        public HelmDoorPanel()
+        {
+            InitializeComponent();
+            DataContext = this;
+            hints = new();
+            foreach(var child in MainGrid.Children)
+            {
+                if(child is HelmDoorHint hint)
+                {
+                    hints.Add(hint);
+                }
+            }
+        }
+
+        public void Reset()
+        {
+            foreach (var element in MainGrid.Children)
+            {
+                if(element is HelmDoorHint hint)
+                {
+                    hint.ItemCount.Text = "?";
+                    hint.DoorItem.SetImage(new BitmapImage(new Uri("../images/dk64/gb.png", UriKind.Relative)));
+                }
+            }
+        }
+
+        public void LoadSavedHelmDoorCounts(List<string> ItemCounts)
+        {
+            for(int i = 0; i < ItemCounts.Count; ++i)
+            {
+                hints[i].ItemCount.Text = ItemCounts[i];
+            }
+        }
+
+        public List<string> GetItemCounts()
+        {
+            var list = new List<string>();
+            foreach(var entry in hints)
+            {
+                list.Add(entry.ItemCount.Text);
+            }
+            return list;
+        }
+
+        public void LoadSavedImageIndexes(List<int> imageIndexes)
+        {
+            for (int i = 0; i < imageIndexes.Count; ++i)
+            {
+                hints[i].DoorItem.SetIndex(imageIndexes[i]);
+            }
+        }
+
+        public List<int> GetImageIndexes()
+        {
+            var list = new List<int>();
+            foreach (var hint in hints)
+            {
+                list.Add(hint.DoorItem.GetIndex());
+            }
+            return list;
+        }
+    }
+}
