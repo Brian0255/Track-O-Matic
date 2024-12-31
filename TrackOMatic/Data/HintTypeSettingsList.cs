b5828efd3721338314c5a293adee084968a12064
Brian0255 <brian.neveu2@gmail.com>
2024-12-30 20:05:17 -0500

    for hint type settings, add a new setting for if the filter/basic sort should be usable/visible

--- TrackOMatic/Data/HintTypeSettingsList.cs
+++ TrackOMatic/Data/HintTypeSettingsList.cs
@@ -7,7 +7,7 @@ namespace TrackOMatic
     public static class HintTypeSettingsList
     {
         public static readonly Dictionary<HintType, HintTypeSettings> SETTINGS = new(){
-            {HintType.PATH, new HintTypeSettings(Visibility.Visible, Visibility.Visible, HintSuggestion.LOCATION) },
+            {HintType.PATH, new HintTypeSettings(Visibility.Visible, Visibility.Visible, HintSuggestion.LOCATION, false, Visibility.Collapsed, Visibility.Visible) },
             {HintType.KONGS, new HintTypeSettings(Visibility.Hidden, Visibility.Visible, HintSuggestion.NONE, true) },
             {HintType.WAY_OF_THE_HOARD, new HintTypeSettings(Visibility.Hidden, Visibility.Visible, HintSuggestion.CHECK) },
             {HintType.REGION_POTION_COUNT, new HintTypeSettings(Visibility.Hidden, Visibility.Hidden, HintSuggestion.LOCATION, false, Visibility.Visible) },
@@ -15,7 +15,7 @@ namespace TrackOMatic
             {HintType.PATHLESS_MOVE, new HintTypeSettings(Visibility.Hidden, Visibility.Hidden, HintSuggestion.MOVE) },
             {HintType.UNHINTED, new HintTypeSettings(Visibility.Hidden, Visibility.Visible, HintSuggestion.CHECK, true) },
             {HintType.MISC, new HintTypeSettings(Visibility.Hidden, Visibility.Hidden, HintSuggestion.NONE) },
-            {HintType.DIRECT_ITEM_HINT, new HintTypeSettings(Visibility.Hidden, Visibility.Visible, HintSuggestion.DIRECT_ITEM_HINT, true, Visibility.Collapsed, Visibility.Visible) },
+            {HintType.DIRECT_ITEM_HINT, new HintTypeSettings(Visibility.Hidden, Visibility.Visible, HintSuggestion.DIRECT_ITEM_HINT, true, Visibility.Collapsed) },
             {HintType.ADVANCED_ITEM_HINT, new HintTypeSettings(Visibility.Hidden, Visibility.Visible, HintSuggestion.LOCATION, false) }
        };
     }
