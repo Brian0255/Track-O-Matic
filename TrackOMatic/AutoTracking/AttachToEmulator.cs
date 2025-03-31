c6ce2c0a99140de37e2ea45cb484103783df3095
Brian0255 <brian.neveu2@gmail.com>
2025-03-31 15:02:07 -0400

    add support for Retroarch/Parallel/PJ64_4.0

--- TrackOMatic/AutoTracking/AttachToEmulator.cs
+++ TrackOMatic/AutoTracking/AttachToEmulator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
+using System.Management;
 using System.Windows.Forms;
 
 namespace TrackOMatic
@@ -20,13 +21,20 @@ namespace TrackOMatic
         }
         private static AttachedProcessInfo AttachToProject64(Process target, GameVerificationInfo verificationInfo)
         {
-            //Project64 versions can differ in memory so try different offsets
-            for (uint potentialOffset = 0xDFD00000; potentialOffset < 0xE01FFFFF; potentialOffset += 16)
+            string filePath = target.MainModule.FileName;
+            FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(filePath);
+            uint lowerBound = 0xDFD00000;
+            uint upperBound = 0xE01F0000;
+            if(versionInfo.FileMajorPart >= 4)
+            {
+                lowerBound = 0xFDD00000;
+                upperBound = 0xFE1FFFFF;
+            }
+            for (uint potentialOffset = lowerBound; potentialOffset < upperBound; potentialOffset += 1)
             {
                 if (Memory.ReadInt32(target, potentialOffset + verificationInfo.TargetAddress) == verificationInfo.TargetValue)
                 {
                     Console.WriteLine(potentialOffset + verificationInfo.TargetAddress);
-                    Console.WriteLine(potentialOffset);
                     return new AttachedProcessInfo(target, potentialOffset);
                 }
             }
@@ -88,14 +96,82 @@ namespace TrackOMatic
             }
             return null;
         }
+
+        private static string GetParentProcessName(Process process)
+        {
+            var myId = process.Id;
+            var query = string.Format("SELECT ParentProcessId FROM Win32_Process WHERE ProcessId = {0}", myId);
+            var search = new ManagementObjectSearcher("root\\CIMV2", query);
+            var results = search.Get().GetEnumerator();
+            results.MoveNext();
+            var queryObj = results.Current;
+            var parentId = (uint)queryObj["ParentProcessId"];
+            var parent = Process.GetProcessById((int)parentId);
+            return parent.ProcessName;
+        }
+
+        private static AttachedProcessInfo RunRetroarchScan(Process target, GameVerificationInfo gameVerificationInfo, ulong addressDLL, uint lowerBound, uint upperBound, uint step, bool isMupen)
+        {
+            for (uint potOff = lowerBound; potOff < upperBound; potOff += step)
+            {
+                ulong romAddrStart = addressDLL + potOff;
+                ulong readAddress = Memory.ReadInt64(target, romAddrStart);
+                if (isMupen)
+                {
+                    readAddress = Memory.ReadInt64(target, (addressDLL + potOff + 4) & readAddress);
+                    readAddress += 0x80000000;
+                }
+
+                var testValue = Memory.ReadInt32(target, (readAddress + gameVerificationInfo.TargetAddress));
+                if ((testValue & 0xFFFFFFFF) == gameVerificationInfo.TargetValue)
+                {
+                    return new AttachedProcessInfo(target, readAddress);
+                }
+            }
+            return null;
+        }
+
+        private static AttachedProcessInfo AttachToRetroarch(Process target, GameVerificationInfo gameVerificationInfo)
+        {
+            ulong addressDLL = 0;
+            bool isMupen = false;
+            foreach (ProcessModule mo in target.Modules)
+            {
+                if (mo.ModuleName.ToLower() == "parallel_n64_next_libretro.dll")
+                {
+                    addressDLL = (ulong)mo.BaseAddress.ToInt64();
+                    break;
+                }
+                else if (mo.ModuleName.ToLower() == "mupen64plus_next_libretro.dll")
+                {
+                    addressDLL = (ulong)mo.BaseAddress.ToInt64();
+                    isMupen = true;
+                    break;
+                }
+            }
+
+            AttachedProcessInfo processInfo;
+            if (addressDLL == 0) return null;
+
+            var parentProcessName = GetParentProcessName(target);
+
+            if (parentProcessName != null && parentProcessName == "parallel-launcher") {
+                processInfo = RunRetroarchScan(target, gameVerificationInfo, addressDLL, 0x845000, 0xD56000, 16, isMupen);
+            }
+            else { 
+                //forcibly set isMupen to false even if it isn't just because retroarch is jank or something
+                processInfo = RunRetroarchScan(target, gameVerificationInfo, addressDLL, 0x000000, 0xFFFFFF, 4, false);
+            }
+
+            return processInfo;
+        }
         public static AttachedProcessInfo Attach(GameVerificationInfo verificationInfo)
         {
             var emu_to_function_call = new Dictionary<string, Func<Process, GameVerificationInfo, AttachedProcessInfo>>()
             {
                 {"project64", AttachToProject64 },
                 {"rmg", AttachToRMG },
-                //something weird keeps this from working?
-                //{"emuhawk", AttachToBizhawk }
+                {"retroarch", AttachToRetroarch }
             };
             foreach (var entry in emu_to_function_call)
             {
